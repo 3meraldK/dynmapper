926234147a513bffd7e4b1a576458a0703bc4fa1
3meraldK <ooyaxcontact@gmail.com>
2024-07-09 04:44:41 +0200

    Update for the new map + handle any order of corners put in

--- dynmapper.py
+++ dynmapper.py
@@ -6,120 +6,125 @@ import time
 import re
 from PIL import Image, ImageDraw
 
-print('')
-
-def hex_to_rgba(hex_str, alpha):
-	r, g, b = (int(hex_str[i:i+2], 16) for i in range(1, 7, 2))
+def hex_to_rgba(hex_string, alpha):
+	r, g, b = (int(hex_string[i:i+2], 16) for i in range(1, 7, 2))
 	return (r, g, b, alpha)
 
-world =  sys.argv[1]
-mode = sys.argv[2]
-scale = float(sys.argv[3])
+def round_to_16(number):
+	return round(number / 16) * 16
+
+def quit(reason):
+	print(reason)
+	exit()
+
+print('')
+
+markers_URL = 'https://map.earthmc.net/tiles/minecraft_overworld/markers.json'
+alliances_api_URL = 'https://emctoolkit.vercel.app/api/aurora/alliances'
+mode = sys.argv[1]
+scale = float(sys.argv[2])
 corners = None
-if len(sys.argv) > 4:
-    corners = eval(sys.argv[4])
-marker_URL = f'https://earthmc.net/map/{world}/standalone/MySQL_markers.php?marker=_markers_/marker_earth.json'
-alliances_api_URL = f'https://emctoolkit.vercel.app/api/{world}/alliances'
 
+# Parse corners
+if len(sys.argv) > 3:
+	if len(sys.argv) < 7:
+		quit('Usage: python dynmapper.py [default/meganation/alliance] [blocks per pixel] (corner coordinates)')
+
+	# Parse corner coordinates
+	coords = sys.argv[3:7]
+	for i, coord in enumerate(coords):
+		try:
+			# Clamping x and z coordinates
+			if i == 0 or i == 2: coords[i] = max(min(int(coord), 33080), -33280)
+			else: coords[i] = max(min(int(coord), 16508), -16640)
+		except ValueError:
+			quit('Corners were not put in correct format, exiting..')
+
+	corners = ((coords[0], coords[1]), (coords[2], coords[3]))
+
+# Parse other unintended inputs
 if scale <= 0:
-	print('Scale must be a positive number, exiting..')
-	exit()
-if world not in ['aurora', 'nova']:
-	print('World must be either nova or aurora, exiting..')
-	exit()
+	quit('Scale must be a positive number, exiting..')
 if mode not in ['default', 'meganation', 'alliance']:
-	print('Wrong mode, exiting..')
-	exit()
+	quit('Wrong mode, exiting..')
 
-print(f'Fetching {mode}s from world {world} at scale 1:{scale:g}')
+print(f'Fetching {mode}s at scale 1:{scale:g}')
 
 if mode != 'default':
-
 	print(f'Fetching alliances from {alliances_api_URL}..')
+
 	response = requests.get(alliances_api_URL)
 	if response.status_code != requests.codes.ok:
-		print('The response status is not OK. Try later, exiting..')
-		exit()
+		quit('The response status is not OK. Try later, exiting..')
+
+	codes = { 'alliance': 'normal', 'meganation': 'mega' }
+
 	data = response.json()
-	arr = []
+	alliances = []
 
-	lookup = {
-		'alliance': 'normal',
-		'meganation': 'mega'
-	}
+	for alliance in data:
+		type = codes.get(mode, mode)
+		if alliance.get('type') != type: continue
+		fill = alliance.get('colours', {}).get('fill', '#000000')
+		outline = alliance.get('colours', {}).get('outline', '#000000')
+		nations = alliance.get('nations', [])
+		alliance_data = [fill, outline, nations]
+		alliances.append(alliance_data)
 
-	for element in data:
-		fetching_req = lookup.get(mode, mode)
-		if element.get('type') == fetching_req:
-			fill = element.get('colours', {}).get('fill', '#000000')
-			outline = element.get('colours', {}).get('outline', '#000000')
-			nations = element.get('nations', [])
-			sub_arr = [fill, outline, nations]
-			arr.append(sub_arr)
+print(f'Fetching markers.json from {markers_URL}..')
 
-print(f'Fetching marker_earth.json from {marker_URL}..')
-
-response = requests.get(marker_URL)
+response = requests.get(markers_URL)
 if response.status_code != requests.codes.ok:
-	print('The response status is not OK. Try later, exiting..')
-	exit()
+	quit('The response status is not OK. Try later, exiting..')
 
 data = response.json()
-areas = data['sets']['townyPlugin.markerset']['areas']
-towns = []
-# Regex pattern to extract nations
-nationPattern = r'>(.*?)<'
+regions = []
 
-for area in areas:
-	fill = areas[area]['fillcolor']
-	# Don't draw shop areas
-	if ' (Shop) (' in areas[area]['desc']:
+for marker in data[0]['markers']:
+	if marker['type'] != 'polygon':
 		continue
 
-	outline = areas[area]['color']
-	x = areas[area]['x']
-	z = areas[area]['z']
-	town = areas[area]['label']
+	nation = re.search(r'\(\b(?:Member|Capital)\b of (.*)\)\n', marker['tooltip'])
+	fill = marker.get('fillColor')
+	outline = marker.get('color')
 
-	# Calculate width and height for image
-	if corners:
-		height = round(abs(corners[0][1] - corners[1][1]) / scale)
-		coords = [(round((x[i] - corners[0][0]) / scale), round((z[i] - corners[0][1]) / scale)) for i in range(len(x))]
-	else:
-		coords = [(round((x[i] + 33280) / scale), round((z[i] + 16640) / scale)) for i in range(len(x))]
+	if fill == None or nation == None: fill = '#000000'
+	if outline == None or nation == None: outline = '#000000'
 
-	if mode != 'default':
-		desc = areas[area]['desc']
-		desc = desc.replace(' (Shop)', '')
-
-		# If town isn't in nation
-		if '()</span><br /> Mayor' in desc:
+	if mode != 'default' and nation != None:
+		nation = nation.group(1)
+		if mode == 'alliance':
 			fill = outline = '#000000'
+		if mode == 'meganation':
+			if fill == '#3FB4FF': fill = outline = '#000000'
+
+		for alliance in arr:
+			if nation not in alliance[2]: continue
+			fill = alliance[0]
+			outline = alliance[1]
+
+	for region in marker['points']:
+		x = []
+		z = []
+		for vertex in region[0]:
+			x.append(round_to_16(vertex['x']))
+			z.append(round_to_16(vertex['z']))
+
+		# Calculate town coordinates
+		if corners:
+			# Extract min and max coordinates to handle any order of provided corners
+			min_x = min(corners[0][0], corners[1][0])
+			max_x = max(corners[0][0], corners[1][0])
+			min_z = min(corners[0][1], corners[1][1])
+			max_z = max(corners[0][1], corners[1][1])
+			coords = [(round((x[i] - min_x) / scale), round((z[i] - min_z) / scale)) for i in range(len(x))]
 		else:
+			coords = [(round((x[i] + 33280) / scale), round((z[i] + 16640) / scale)) for i in range(len(x))]
 
-			# Extract nation from town's description
-			start_idx = desc.find('(')
-			end_idx = desc.find(')')
-			if start_idx != -1 and end_idx != -1:
-				nation = desc[start_idx + 1:end_idx]
+		region = {'fill': fill, 'outline': outline, 'coords': coords}
+		regions.append(region)
 
-			# If nation still contains </a>
-			if '</a>' in nation:
-				nation = re.search(nationPattern, nation).group(1)
-
-			if mode == 'alliance':
-				fill = outline = '#000000'
-			if mode == 'meganation':
-				if fill == '#3FB4FF': fill = outline = '#000000'
-
-			for alliance in arr:
-				if nation in alliance[2]:
-					fill = alliance[0]
-					outline = alliance[1]
-
-	town = {'fill': fill, 'outline': outline, 'coords': coords}
-	towns.append(town)
-
+# Calculate image size
 if corners:
 	width = round(abs(corners[0][0] - corners[1][0]) / scale)
 	height = round(abs(corners[0][1] - corners[1][1]) / scale)
@@ -127,11 +132,15 @@ else:
 	width = round(66360 / scale)
 	height = round(33148 / scale)
 
+if width <= 0 or height <= 0:
+    quit('Width or height of image is not positive, exiting..')
+
 print(f'Creating image of {width} x {height} px in current directory..')
 
 image = Image.new('RGBA', (width, height), (0, 0, 0, 0))
 draw = ImageDraw.Draw(image)
-[ draw.polygon(town['coords'], hex_to_rgba(town['fill'], 51), hex_to_rgba(town['outline'], 255)) for town in towns ]
-file_name = f'{mode}-{world}-{int(time.time())}.png'
+[ draw.polygon(region['coords'], hex_to_rgba(region['fill'], 51), hex_to_rgba(region['outline'], 255)) for region in regions ]
+
+file_name = f'{mode}-{int(time.time())}.png'
 image.save(file_name)
 print(f'Saved image as {file_name}, exiting..')
